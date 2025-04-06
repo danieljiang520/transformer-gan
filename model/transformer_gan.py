9f669c426c207a8709cfc0c2df65aa2b7d9c3406
danieljiang520 <danieldj@stanford.edu>
2025-04-06 09:51:09 -0700

    d

--- model/transformer_gan.py
+++ model/transformer_gan.py
@@ -1,5 +1,5 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-  
+
 # Licensed under the Apache License, Version 2.0 (the "License").
 # You may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -25,7 +25,7 @@ from transformers import (
     BertForMaskedLM,
     PreTrainedTokenizer,
     PreTrainedModel,
-    AdamW,
+    # AdamW,
     BertForSequenceClassification
 )
 
